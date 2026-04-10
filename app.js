2ad31a17e6e33fecdfebee593fd33657012bceaf
ariadna-gh <ariadna.yakovlev@krfk.kai.edu.ua>
2026-04-10 19:23:03 +0300

    feat: broken code for CI demo

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const statusBox = document.getElementById('status')
 const checkBtn = document.getElementById('check-btn')
 
+// помилка 1
+const unusedVariable = 'я ніде не використовуюсь'
+
+// помилка 2
 function getStatus() {
   const now = new Date()
   const timeString = now.toLocaleTimeString('uk-UA')
+
+  if (timeString == null) { // ❌ має бути ===
+    return
+  }
+
   statusBox.textContent = `✅ Все працює. Перевірено о ${timeString}`
 }
 
 checkBtn.addEventListener('click', getStatus)
-
 getStatus()
